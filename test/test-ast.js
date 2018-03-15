ae0fd00c7b049963724460000a8a22c097c34a77
jtorre39 <jtorre39@lion.lmu.edu>
2018-03-14 23:11:13 -0700

    Begun skeleton for ast tests.

--- test/test-ast.js
+++ test/test-ast.js
@@ -4,6 +4,27 @@ const parse = require('../syntax/parser');
 
 
 let file = parse("lulu := true");
-const util = require('util')
+const util = require('util');
 
-console.log(util.inspect(file, {showHidden: false, depth: null}))
+console.log(util.inspect(file, {showHidden: false, depth: null}));
+/* eslint-enable no-undef */
+describe('Declarations', () => {
+  const grammar = LegalVisaCardNumberGrammar;
+  it ("correctly parse declarations", () => {
+    const testAST = {
+      Program: {
+        Body: {
+          statements:
+        }
+      }
+    };
+
+  });
+
+  it ("should fail parsing", ()=> {
+    expect(parse(grammar, "510510510510510")).toBe(false);
+    expect(parse(grammar, "4105105105105101")).toBe(false);
+    expect(parse(grammar, "4")).toBe(false);
+    expect(parse(grammar, "")).toBe(false);
+  });
+});
