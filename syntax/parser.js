93501af3a333c55294254f957690dd588887c42f
yshin4 <yshin4@lion.lmu.edu>
2018-03-15 00:16:08 -0700

    ast,parser added numlit, stringlit, unary, expressions0-7, while

--- syntax/parser.js
+++ syntax/parser.js
@@ -17,35 +17,43 @@ const Body = require('../ast/body');
 const VarDec = require('../ast/variable-declaration');
 const VarAsgn = require('../ast/assignment-statement');
 const BooleanLiteral = require('../ast/boolean-literal');
-
-// const BinaryExpression = require('../ast/binary-expression.js');
+const NumericLiteral = require('../ast/numeric-literal');
+const StringLiteral = require('../ast/string-literal');
+const WhileStatement = require('../ast/while-statement');
+const BinaryExpression = require('../ast/binary-expression.js');
+const UnaryExpression = require('../ast/unary-expression.js');
+const SubscriptedExpression = require('../ast/subscripted-expression');
 // const TernaryExpression = require('../ast/ternary-expression.js');
 
 const grammar = ohm.grammar(fs.readFileSync('./syntax/jen.ohm'));
 const astGenerator = grammar.createSemantics().addOperation('ast', {
   Program (_1, body, _2) { return new Program(body.ast()); },
   Body (_1, expressionsAndStatements, _2) { return new Body(expressionsAndStatements.ast()); },
+  Suite (_1, _2, body, _3) { return body.ast(); },
+
   Declaration (ids, _, exps) { return new VarDec(ids.ast(), exps.ast()); },
   Assignment (ids, _, exps) { return new VarAsgn(ids.ast(), exps.ast()); },
+  While (_1, exps, _2, suite) { return new WhileStatement(exps.ast(), suite.ast()); },
+  Exp0_and (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp0_or (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp0_xor (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp1_binary (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp2_binary (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp3_binary (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp4_binary (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp5_not (op, operand) { return new UnaryExpression(op.ast(), operand.ast()); },
+  Exp6_binary (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp7_parens (_1, expression, _2) { return expression.ast(); },
+
+  // SubscriptExp (id, _1, expression, _2) { return new SubscriptedExpression(id.ast(), expression.ast()); },
   NonemptyListOf (first, _, rest) { return [first.ast(), ...rest.ast()]; },
   varId (_1, _2) { return this.sourceString; },
   constId (_1, _2) { return this.sourceString; },
   packageId (_1, _2) { return this.sourceString; },
   booleanLiteral (_) { return new BooleanLiteral(!!this.sourceString); },
+  numLiteral (_) { return new NumericLiteral(+this.sourceString); },
+  stringLiteral (_1, chars, _2) { return new StringLiteral(this.sourceString); },
   _terminal () { return this.sourceString; },
-  // Assignment () {}
-
-
-
-  // // vThis might be right - Thomas
-  // Exp_ternary(left, _1, middle, _2, right) { return new TernaryExpression(left.ast(), middle.ast() right.ast()); },
-  // Exp1_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  // Exp2_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  // Exp3_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  // Exp4_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  // // ^This might be right - Thomas
-
-  // id (name) { return ; }
 });
 
 module.exports = (text) => {
--- test/manual-test-ast.js
+++ test/manual-test-ast.js
@@ -6,14 +6,17 @@ const parse = require('../syntax/parser');
 let dec = parse("sad := true");
 
 // const declaration
-let constDec = parse("RICH := 12");
+let constDec = parse("RICH := (4 + 3) * 2");
 
 // assignment
-let ass = parse("sad = \"woof\"")
+let ass = parse("sad = \"woof\"");
 
+// or
+let or = parse("big := (3 > 1) || !true");
 
-const util = require('util')
+const util = require('util');
 
 console.log(util.inspect(dec, {showHidden: false, depth: null}));
 console.log(util.inspect(constDec, {showHidden: false, depth: null}));
 console.log(util.inspect(ass, {showHidden: false, depth: null}));
+console.log(util.inspect(or, {showHidden: false, depth: null}));
