4eb3e8110a7cda803a8513a214f6d607688a5f40
jtorre39 <jtorre39@lion.lmu.edu>
2018-03-16 20:00:19 -0700

    Linted parser.js

--- syntax/parser.js
+++ syntax/parser.js
@@ -38,53 +38,62 @@ const SumTypeClass = require('../ast/sum-type');
 
 // Credit to Ray Toal:
 // Ohm turns `x?` into either [x] or [], which we should clean up for our AST.
-const unpack = (a) => a.length === 0 ? null : a[0];
+const unpack = a => (a.length === 0 ? null : a[0]);
 
 const grammar = ohm.grammar(fs.readFileSync('./syntax/jen.ohm'));
+/* eslint-disable no-unused-vars */
 const astGenerator = grammar.createSemantics().addOperation('ast', {
-  Program (_1, body, _2) { return new Program(body.ast()); },
-  Body (_1, expressionsAndStatements, _2) { return new Body(expressionsAndStatements.ast()); },
-  Suite (_1, _2, body, _3) { return body.ast(); },
-  Conditional (_1, firstTest, _2, _3, firstSuite, _4, moreTests, _5, _6, moreSuites, _7, _8, _9, lastSuite) {
+  Program(_1, body, _2) { return new Program(body.ast()); },
+  Body(_1, expressionsAndStatements, _2) { return new Body(expressionsAndStatements.ast()); },
+  Suite(_1, _2, body, _3) { return body.ast(); },
+  Conditional(_1, firstTest, _2, _3, firstSuite, _4, moreTests, _5, _6, moreSuites, _7, _8, _9, lastSuite) {
     const tests = [firstTest.ast(), ...moreTests.ast()];
     const bodies = [firstSuite.ast(), ...moreSuites.ast()];
     const cases = tests.map((test, index) => new Case(test, bodies[index]));
     return new IfStatement(cases, unpack(lastSuite.ast()));
   },
-  Declaration (ids, _, exps) { return new VarDec(ids.ast(), exps.ast()); },
-  Assignment (ids, _, exps) { return new VarAsgn(ids.ast(), exps.ast()); },
-  For (_1, exps, _2, e, _3, suite) { return new ForStatement(exps.ast(), e.ast(), suite.ast()); },
-  While (_1, exps, _2, suite) { return new WhileStatement(exps.ast(), suite.ast()); },
-  TypeDec (_1, id, sumType) { return new TypeDeclaration(id.ast(), sumType.ast()); },
-  ReturnExp (_, e) { return new Return(unpack(e.ast())); },
-  // FuncDec(annotation, _1, signature, _2, suite) { return new FunctionDeclaration(id.ast(), params.ast(), suite.ast()); },
-  Expression_ternary (conditional, _1, trueValue, _2, falseValue) { return new TernaryExpression(conditional.ast(), trueValue.ast(), falseValue.ast()); },
-  Exp0_and (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp0_or (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp0_xor (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp1_binary (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp2_binary (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp3_binary (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp4_binary (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp5_not (op, operand) { return new UnaryExpression(op.ast(), operand.ast()); },
-  Exp6_accessor (object, _1, property, _2) { return new Accessor(object.ast(), property.ast()); },
-  Exp6_binary (left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp7_parens (_1, expression, _2) { return expression.ast(); },
+  Declaration(ids, _, exps) { return new VarDec(ids.ast(), exps.ast()); },
+  Assignment(ids, _, exps) { return new VarAsgn(ids.ast(), exps.ast()); },
+  For(_1, exps, _2, e, _3, suite) { return new ForStatement(exps.ast(), e.ast(), suite.ast()); },
+  While(_1, exps, _2, suite) { return new WhileStatement(exps.ast(), suite.ast()); },
+  TypeDec(_1, id, sumType) { return new TypeDeclaration(id.ast(), sumType.ast()); },
+  ReturnExp(_, e) { return new Return(unpack(e.ast())); },
+  // FuncDec(annotation, _1, signature, _2, suite) {
+  //   return new FunctionDeclaration(id.ast(), params.ast(), suite.ast());
+  // },
+  Expression_ternary(conditional, _1, trueValue, _2, falseValue) {
+    return new TernaryExpression(conditional.ast(), trueValue.ast(), falseValue.ast());
+  },
+  Exp0_and(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp0_or(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp0_xor(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp1_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp2_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp3_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp4_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp5_not(op, operand) { return new UnaryExpression(op.ast(), operand.ast()); },
+  Exp6_accessor(object, _1, property, _2) { return new Accessor(object.ast(), property.ast()); },
+  Exp6_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
+  Exp7_parens(_1, expression, _2) { return expression.ast(); },
 
-  List (_1, values, _2) { return new ListExpression(values.ast()); },
-  ListType (_1, type) { return new ListTypeExpression(type.ast()); },
-  SumType (basicTypeOrId1, _1, moreBasicTypeOrId1) { return new SumTypeClass(basicTypeOrId1.ast(), moreBasicTypeOrId1.ast()); },
-  FuncCall (callee, _1, args, _2) { return new FunctionCall(callee.ast(), args.ast()); },
-  SubscriptExp (id, _1, expression, _2) { return new SubscriptedExpression(id.ast(), expression.ast()); },
-  NonemptyListOf (first, _, rest) { return [first.ast(), ...rest.ast()]; },
-  varId (_1, _2) { return this.sourceString; },
-  constId (_1, _2) { return this.sourceString; },
-  packageId (_1, _2) { return this.sourceString; },
-  booleanLiteral (_) { return new BooleanLiteral(!!this.sourceString); },
-  numLiteral (_) { return new NumericLiteral(+this.sourceString); },
-  errLiteral (_) { return new ErrorLiteral(this.sourceString); },
-  stringLiteral (_1, chars, _2) { return new StringLiteral(this.sourceString); },
-  _terminal () { return this.sourceString; }
+  List(_1, values, _2) { return new ListExpression(values.ast()); },
+  ListType(_1, type) { return new ListTypeExpression(type.ast()); },
+  SumType(basicTypeOrId1, _1, moreBasicTypeOrId1) {
+    return new SumTypeClass(basicTypeOrId1.ast(), moreBasicTypeOrId1.ast());
+  },
+  FuncCall(callee, _1, args, _2) { return new FunctionCall(callee.ast(), args.ast()); },
+  SubscriptExp(id, _1, expression, _2) {
+    return new SubscriptedExpression(id.ast(), expression.ast());
+  },
+  NonemptyListOf(first, _, rest) { return [first.ast(), ...rest.ast()]; },
+  varId(_1, _2) { return this.sourceString; },
+  constId(_1, _2) { return this.sourceString; },
+  packageId(_1, _2) { return this.sourceString; },
+  booleanLiteral(_) { return new BooleanLiteral(!!this.sourceString); },
+  numLiteral(_) { return new NumericLiteral(+this.sourceString); },
+  errLiteral(_) { return new ErrorLiteral(this.sourceString); },
+  stringLiteral(_1, chars, _2) { return new StringLiteral(this.sourceString); },
+  _terminal() { return this.sourceString; },
 });
 
 module.exports = (text) => {
