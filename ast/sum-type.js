0f274ed4fdf4d193b81c5354e168c97d496361ca
Anthony Keba <Anthony.Keba@FiveTalent.com>
2018-03-16 17:13:07 -0700

    Added file for SumType AST

---
+++ ast/sum-type.js
@@ -0,0 +1,10 @@
+module.exports = class SumType {
+  constructor(targets, sources){
+    Object.assign(this, { id, value });
+  }
+
+  analyze(context) {
+    this.value = this.value.optimize();
+    if(r
+  }
+};
