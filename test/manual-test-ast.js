bbe2db3c6f19e23069e25fffa1da2f159e248eae
yshin4 <yshin4@lion.lmu.edu>
2018-03-20 01:09:24 -0700

    added logicalops test ast

--- test/manual-test-ast.js
+++ test/manual-test-ast.js
@@ -1,23 +1,23 @@
-/* eslint-disable */
-
-const fs = require('fs');
-const parse = require('../syntax/parser');
-
-// declaration
-const dec = parse('sad := true && false');
-
-// const declaration
-const constDec = parse('RICH := (4 + 3) * 2');
-
-// assignment
-const ass = parse('sad = "woof"');
-
-// or
-const or = parse('big := (3 > 1) || !true');
-
-const util = require('util');
-
-console.log(util.inspect(dec, { showHidden: false, depth: null }));
-console.log(util.inspect(constDec, { showHidden: false, depth: null }));
-console.log(util.inspect(ass, { showHidden: false, depth: null }));
-console.log(util.inspect(or, { showHidden: false, depth: null }));
+// /* eslint-disable */
+//
+// const fs = require('fs');
+// const parse = require('../syntax/parser');
+//
+// // declaration
+// const dec = parse('sad := true && false');
+//
+// // const declaration
+// const constDec = parse('RICH := (4 + 3) * 2');
+//
+// // assignment
+// const ass = parse('sad = "woof"');
+//
+// // or
+// const or = parse('big := (3 > 1) || !true');
+//
+// const util = require('util');
+//
+// console.log(util.inspect(dec, { showHidden: false, depth: null }));
+// console.log(util.inspect(constDec, { showHidden: false, depth: null }));
+// console.log(util.inspect(ass, { showHidden: false, depth: null }));
+// console.log(util.inspect(or, { showHidden: false, depth: null }));
--- test/test-ast-logicalop.js
+++ test/test-ast-logicalop.js
@@ -19,47 +19,56 @@ describe('logicalop', () => {
     },
   };
 
-
   it('should correctly parse and &&', () => {
-    expected.body.statements[0].ids[0] = 'and_test';
+    expected.body.statements[0].ids[0] = 'and_test01';
     expected.body.statements[0].initializers[0].op = '&&';
-    let result = parse('and_test := true && true');
+    let result = parse('and_test01 := true && true');
     assert.deepEqual(result, expected);
 
-    expected.body.statements[0].ids[0] = 'and_test';
-    expected.body.statements[0].initializers[0].op = '&&';
+    expected.body.statements[0].ids[0] = 'and_test02';
     expected.body.statements[0].initializers[0].left = { value: false };
-    result = parse('and_test = false && true');
+    result = parse('and_test02 := false && true');
     assert.deepEqual(result, expected);
 
-    // expected.body.statements[0].ids[0] = 'and_test';
-    // expected.body.statements[0].initializers[0].op = '&&';
-    // expected.body.statements[0].initializers[0].right = { value: false };
-    // result = parse('and_test = false && false');
-    // assert.deepEqual(result, expected);
+    expected.body.statements[0].ids[0] = 'and_test03';
+    expected.body.statements[0].initializers[0].right = { value: false };
+    result = parse('and_test03 := false && false');
+    assert.deepEqual(result, expected);
   });
-  //
-  // it('should correctly parse constants - constId', () => {
-  //   expected.body.statements[0].ids[0] = 'MARCH14';
-  //   let result = parse('MARCH14 := true');
-  //   assert.deepEqual(result, expected);
-  //
-  //   expected.body.statements[0].ids[0] = 'PIE_DAY';
-  //   result = parse('PIE_DAY := true');
-  //   assert.deepEqual(result, expected);
-  // });
-  //
-  // it('should correctly parse package ids - packageId', () => {
-  //   expected.body.statements[0].ids[0] = 'P_ackage';
-  //   let result = parse('P_ackage := true');
-  //   assert.deepEqual(result, expected);
-  //
-  //   expected.body.statements[0].ids[0] = 'SWELL2018';
-  //   result = parse('SWELL2018 := true');
-  //   assert.deepEqual(result, expected);
-  //
-  //   expected.body.statements[0].ids[0] = 'D__________';
-  //   result = parse('D__________ := true');
-  //   assert.deepEqual(result, expected);
-  // });
+
+  it('should correctly parse or ||', () => {
+    expected.body.statements[0].ids[0] = 'or_test01';
+    expected.body.statements[0].initializers[0].op = '||';
+    let result = parse('or_test01 := false || false');
+    assert.deepEqual(result, expected);
+
+    expected.body.statements[0].ids[0] = 'or_test02';
+    expected.body.statements[0].initializers[0].left = { value: true };
+    result = parse('or_test02 := true || false');
+    assert.deepEqual(result, expected);
+
+    expected.body.statements[0].ids[0] = 'or_test03';
+    expected.body.statements[0].initializers[0].right = { value: true };
+    result = parse('or_test03 := true || true');
+    assert.deepEqual(result, expected);
+  });
+
+  it('should correctly parse xor &!&', () => {
+    expected.body.statements[0].ids[0] = 'xor_test01';
+    expected.body.statements[0].initializers[0].op = '&!&';
+    expected.body.statements[0].initializers[0].left = { value: 1 };
+    expected.body.statements[0].initializers[0].right = { value: 1 };
+    let result = parse('xor_test01 := 1 &!& 1');
+    assert.deepEqual(result, expected);
+
+    expected.body.statements[0].ids[0] = 'xor_test02';
+    expected.body.statements[0].initializers[0].left = { value: 0 };
+    result = parse('xor_test02 := 0 &!& 1');
+    assert.deepEqual(result, expected);
+
+    expected.body.statements[0].ids[0] = 'xor_test03';
+    expected.body.statements[0].initializers[0].right = { value: 0 };
+    result = parse('xor_test03 := 0 &!& 0');
+    assert.deepEqual(result, expected);
+  });
 });
