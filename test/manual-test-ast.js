17aee21176f253255e24574ad5dbdf8ea0d4640d
jtorre39 <jtorre39@lion.lmu.edu>
2018-03-20 00:18:34 -0700

    Undid changes to manual-test-ast.js

--- test/manual-test-ast.js
+++ test/manual-test-ast.js
@@ -17,23 +17,6 @@
 //
 // const util = require('util');
 
-<<<<<<< HEAD
-// declaration
-const dec = parse('sad := true');
-
-// const declaration
-const constDec = parse('RICH := (4 + 3) * 2');
-
-// assignment
-const ass = parse('sad = "woof"');
-
-// or
-const or = parse('big := (3 > 1) || !true');
-
-const util = require('util');
-
-=======
->>>>>>> 54dc0300f379c35744b29956672e8323b9fd9cce
 // console.log(util.inspect(dec, { showHidden: false, depth: null }));
 // console.log(util.inspect(constDec, { showHidden: false, depth: null }));
 // console.log(util.inspect(ass, { showHidden: false, depth: null }));
