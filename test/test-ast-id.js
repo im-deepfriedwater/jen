1ff81412ae4f177f9cc25443af565484e2eb1fde
yshin4 <yshin4@lion.lmu.edu>
2018-03-19 23:58:05 -0700

    finished vardid, constid packageid ast test

--- test/test-ast-id.js
+++ test/test-ast-id.js
@@ -1,7 +1,6 @@
 const parse = require('../syntax/parser');
 const assert = require('assert');
 
-const astCompare = (x, y) => JSON.stringify(x) === JSON.stringify(y);
 /* eslint-disable no-undef */
 describe('id', () => {
   const expected = {
@@ -15,20 +14,46 @@ describe('id', () => {
     },
   };
 
-  it('should correctly parse varId', () => {
+  it('should correctly parse variables - varId', () => {
     expected.body.statements[0].ids[0] = 'lowerUPPER';
     expected.body.statements[0].initializers[0] = { value: true };
     let result = parse('lowerUPPER := true');
-    assert.equal(astCompare(expected, result), true);
+    assert.deepEqual(result, expected);
 
     expected.body.statements[0].ids[0] = '_jen01';
-    expected.body.statements[0].initializers[0] = { value: true };
     result = parse('_jen01 := true');
-    assert.equal(astCompare(expected, result), true);
+    assert.deepEqual(result, expected);
 
-    expected.body.statements[0].ids[0] = 'CONSTANT';
-    expected.body.statements[0].initializers[0] = { value: true };
-    result = parse('CONSTANT := true');
-    assert.equal(astCompare(expected, result), false);
+    expected.body.statements[0].ids[0] = '_1337';
+    result = parse('_1337 := true');
+    assert.deepEqual(result, expected);
+
+    expected.body.statements[0].ids[0] = 'o__o';
+    result = parse('o__o := true');
+    assert.deepEqual(result, expected);
+  });
+
+  it('should correctly parse constants - constId', () => {
+    expected.body.statements[0].ids[0] = 'MARCH14';
+    let result = parse('MARCH14 := true');
+    assert.deepEqual(result, expected);
+
+    expected.body.statements[0].ids[0] = 'PIE_DAY';
+    result = parse('PIE_DAY := true');
+    assert.deepEqual(result, expected);
+  });
+
+  it('should correctly parse package ids - packageId', () => {
+    expected.body.statements[0].ids[0] = 'P_ackage';
+    let result = parse('P_ackage := true');
+    assert.deepEqual(result, expected);
+
+    expected.body.statements[0].ids[0] = 'SWELL2018';
+    result = parse('SWELL2018 := true');
+    assert.deepEqual(result, expected);
+
+    expected.body.statements[0].ids[0] = 'D__________';
+    result = parse('D__________ := true');
+    assert.deepEqual(result, expected);
   });
 });
