7508618c305cf2fbbea8a338f0dcd4a494934959
eshen1 <eshen1@lion.lmu.edu>
2018-03-20 17:05:03 -0700

    added more list tests

--- test/test-ast-list.js
+++ test/test-ast-list.js
@@ -6,14 +6,35 @@ describe('List', () => {
     body: {
       statements: [
         {
-            values: ['bread', 'cheese', 'wine'],
+            values: [],
         },
       ],
     },
   };
 
+beforeEach(() => {
+    // Clear out the test object before each run.
+  expected.body.statements[0] = {
+    values: [],
+  };
+});
+
   it('should correctly parse a list', () => {
+    expected.body.statements[0].values = ['bread', 'cheese', 'wine'];
     const result = parse('[bread, cheese, wine]');
     assert.deepEqual(result, expected);
   });
+
+  it('should correctly parse a list', () => {
+    expected.body.statements[0].values = ['crawl', 'run', 'boolean'];
+    const result = parse('[crawl, run, boolean]');
+    assert.deepEqual(result, expected);
+  });
+
+  it('should correctly parse an empty list', () => {
+    expected.body.statements[0].values = [];
+    const result = parse('[]');
+    assert.deepEqual(result, expected);
+  });
+
 });
