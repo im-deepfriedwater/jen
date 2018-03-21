df5c9dfa51acad87f708cea1d82ac2965ec46416
eshen1 <eshen1@lion.lmu.edu>
2018-03-21 13:28:34 -0700

    added more list tests

--- test/test-ast-list.js
+++ test/test-ast-list.js
@@ -22,15 +22,41 @@ describe('List', () => {
     assert.deepEqual(result, expected);
   });
 
-  it('should correctly parse a list', () => {
-    expected.body.statements[0].values = ['crawl', 'run', 'boolean'];
-    const result = parse('[crawl, run, boolean]');
+  it('should correctly parse a list of numbers', () => {
+    expected.body.statements[0].values = [
+      { value: 1 },
+      { value: 5 },
+      { value: 0 },
+      { value: 2 },
+      { value: 2 },
+      { value: 2 },
+    ];
+    const result = parse('[1, 5, 0, 2, 2, 2]');
     assert.deepEqual(result, expected);
   });
 
   it('should correctly parse an empty list', () => {
     expected.body.statements[0].values = [];
     const result = parse('[]');
+    assert.deepEqual(result, expected);
+  });
+
+  it('should correctly parse a list of different types', () => {
+    expected.body.statements[0].values = [
+      { value: true },
+      {
+        left: { value: 1 },
+        op: '+',
+        right: { value: 1 },
+      },
+      {
+        args: [],
+        callee: 'swim',
+      },
+      { value: 0 },
+      { value: 34 },
+    ];
+    const result = parse('[true, 1+1, swim(), 0, 34]');
     assert.deepEqual(result, expected);
   });
 });
