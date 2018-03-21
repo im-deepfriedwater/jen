8b8466aa1b48feba70faec5d53b55f6c3b04bbb8
eshen1 <eshen1@lion.lmu.edu>
2018-03-21 13:52:15 -0700

    added more tests to while

--- test/test-ast-while.js
+++ test/test-ast-while.js
@@ -22,4 +22,29 @@ describe('While', () => {
     const result = parse('while true : \n  sheep := ba');
     assert.deepEqual(result, expected);
   });
+  it('should correctly parse a while loop with multiple conditionals', () => {
+    expected.body.statements[0].test = {
+      left: {
+        left: {
+          left: 'a',
+          op: '==',
+          right: 'b',
+        },
+        op: '&&',
+        right: {
+          left: 'grass',
+          op: '==',
+          right: 'green',
+        },
+      },
+      op: '&&',
+      right: {
+        left: 'sky',
+        op: '!=',
+        right: 'blue',
+      },
+    };
+    const result = parse('while a == b && grass == green && sky != blue  : \n  sheep := ba');
+    assert.deepEqual(result, expected);
+  });
 });
