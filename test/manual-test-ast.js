a3886e342614c51195db2a233699184e4d1065c0
jtorre39 <jtorre39@lion.lmu.edu>
2018-03-20 23:49:31 -0700

    Commented out extra test file, all ast tests passing!

--- test/manual-test-ast.js
+++ test/manual-test-ast.js
@@ -1,27 +1,27 @@
-// /* eslint-disable */
-//
-const fs = require('fs');
- const parse = require('../syntax/parser');
-//
-// // not
-// const not = parse('bang := !true');
-//
-// // declaration
-// const dec = parse('sad := true && false');
-//
-// // const declaration
-// const constDec = parse('RICH := (4 + 3 + 1) * 2');
-//
-// // assignment
-// const ass = parse('sad = "woof"');
-//
-// // or
- const or = parse('[bread, steam, rice]');
-//
- const util = require('util');
-//
-// console.log(util.inspect(not, { showHidden: false, depth: null }));
-// console.log(util.inspect(dec, { showHidden: false, depth: null }));
-// console.log(util.inspect(constDec, { showHidden: false, depth: null }));
-// console.log(util.inspect(ass, { showHidden: false, depth: null }));
-console.log(util.inspect(or, { showHidden: false, depth: null }));
+// // /* eslint-disable */
+// //
+// const fs = require('fs');
+//  const parse = require('../syntax/parser');
+// //
+// // // not
+// // const not = parse('bang := !true');
+// //
+// // // declaration
+// // const dec = parse('sad := true && false');
+// //
+// // // const declaration
+// // const constDec = parse('RICH := (4 + 3 + 1) * 2');
+// //
+// // // assignment
+// // const ass = parse('sad = "woof"');
+// //
+// // // or
+//  const or = parse('[bread, steam, rice]');
+// //
+//  const util = require('util');
+// //
+// // console.log(util.inspect(not, { showHidden: false, depth: null }));
+// // console.log(util.inspect(dec, { showHidden: false, depth: null }));
+// // console.log(util.inspect(constDec, { showHidden: false, depth: null }));
+// // console.log(util.inspect(ass, { showHidden: false, depth: null }));
+// console.log(util.inspect(or, { showHidden: false, depth: null }));
