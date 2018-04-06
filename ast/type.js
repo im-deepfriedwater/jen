6eeeb59e4b8b5dbd71741a26c587c69665518add
jtorre39 <jtorre39@lion.lmu.edu>
2018-04-05 19:25:21 -0700

    Implemented type.js

---
+++ ast/type.js
@@ -0,0 +1,40 @@
+class Type {
+  constructor(name) {
+    this.name = name;
+    Type.cache[name] = this;
+  }
+  mustBeNumber(message) {
+    return this.mustBeCompatibleWith(Type.NUMBER, message);
+  }
+  mustBeBoolean(message) {
+    return this.mustBeCompatibleWith(Type.BOOLEAN, message);
+  }
+  mustBeCompatibleWith(otherType, message) {
+    if (!this.isCompatibleWith(otherType)) {
+      throw message;
+    }
+  }
+  mustBeMutuallyCompatibleWith(otherType, message) {
+    if (!(this.isCompatibleWith(otherType) || otherType.isCompatibleWith(this))) {
+      throw message;
+    }
+  }
+  isCompatibleWith(otherType) {
+    // In more sophisticated languages, comapatibility would be more complex
+    return this === otherType;
+  }
+}
+
+Type.cache = {};
+
+Type.BOOLEAN = new Type('boolean');
+Type.NUMBER = new Type('number');
+Type.ERROR = new Type('error');
+Type.STRING = new Type('string');
+Type.VOID = new Type('void');
+Type.ANY = new Type('any');
+
+Type.forName = name => Type.cache[name];
+
+
+module.exports = Type;
