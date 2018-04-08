bd772e690ff07b1a7bfac8b60b13d6b8031b1565
eshen1 <eshen1@lion.lmu.edu>
2018-04-08 14:47:27 -0700

    added analyze method for break

--- ast/break.js
+++ ast/break.js
@@ -3,8 +3,10 @@ module.exports = class BreakStatement {
     // Intentionally empty
   }
 
-  analyze() { // eslint-disable-line class-methods-use-this
-    // Intentionally empty
+  analyze(context) { // eslint-disable-line class-methods-use-this
+    if (!context.inLoop) {
+      throw new Error('Break statement outside loop');
+    }
   }
 
   optimize() {
