1a74b2919558515028a40c704aa2f58dcb758be3
yshin4 <yshin4@lion.lmu.edu>
2018-04-19 18:10:45 -0700

    added number of arguments for function call check

--- ast/function-call.js
+++ ast/function-call.js
@@ -11,26 +11,33 @@ module.exports = class Call {
   }
 
   checkArgumentMatching(callee) {
-    let keywordArgumentSeen = false;
-    const matchedParameterNames = new Set([]);
-    this.args.forEach((arg, index) => {
-      if (index >= callee.params.length) {
-        throw new Error('Too many arguments in call');
-      }
-      if (arg.isKeywordArgument) {
-        keywordArgumentSeen = true;
-      } else if (keywordArgumentSeen) {
-        throw new Error('Positional argument in call after keyword argument');
-      }
-      const parameterName = arg.id ? arg.id : callee.params[index].id;
-      if (!callee.allParameterNames.has(parameterName)) {
-        throw new Error(`Function does not have a parameter called ${parameterName}`);
-      }
-      if (matchedParameterNames.has(parameterName)) {
-        throw new Error(`Multiple arguments for parameter ${parameterName}`);
-      }
-      matchedParameterNames.add(parameterName);
-    });
+    if (this.args.length > callee.params.length) {
+      throw new Error('Too many arguments in call');
+    } else if (this.args.length < callee.params.length) {
+      throw new Error('Too little arguments in call');
+    }
+
+    // this.args[0].type === callee.params[0].type
+    //
+    //
+    // this.args.forEach((arg, index) => {
+    //   if (index >= callee.params.length) {
+    //     throw new Error('Too many arguments in call');
+    //   }
+    //   if (arg.isKeywordArgument) {
+    //     keywordArgumentSeen = true;
+    //   } else if (keywordArgumentSeen) {
+    //     throw new Error('Positional argument in call after keyword argument');
+    //   }
+    //   const parameterName = arg.id ? arg.id : callee.params[index].id;
+    //   if (!callee.allParameterNames.has(parameterName)) {
+    //     throw new Error(`Function does not have a parameter called ${parameterName}`);
+    //   }
+    //   if (matchedParameterNames.has(parameterName)) {
+    //     throw new Error(`Multiple arguments for parameter ${parameterName}`);
+    //   }
+    //   matchedParameterNames.add(parameterName);
+    // });
   }
 
   optimize() {
