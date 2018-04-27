11e3ab8787a60c26e7cb87c6fae4315138725540
yshin4 <yshin4@lion.lmu.edu>
2018-04-26 18:57:27 -0700

    added type inference for binary expression

--- ast/binary-expression.js
+++ ast/binary-expression.js
@@ -1,3 +1,5 @@
+const Type = require('./type');
+
 module.exports = class BinaryExpression {
   constructor(op, left, right) {
     Object.assign(this, { op, left, right });
@@ -6,6 +8,36 @@ module.exports = class BinaryExpression {
   analyze(context) {
     this.left.analyze(context);
     this.right.analyze(context);
+    console.log(this.left)
+    if (['<=', '>=', '>', '<'].includes(this.op)) {
+      this.mustHaveIntegerOperands();
+      this.type = Type.BOOLEAN;
+    } else if (['==', '!='].includes(this.op)) {
+      this.mustHaveCompatibleOperands();
+      this.type = Type.BOOLEAN;
+    } else if (['&&', '||', '&!&'].includes(this.op)) {
+      this.mustHaveBooleanOperands();
+      this.type = Type.BOOLEAN;
+    } else {
+      // All other binary operators are arithmetic
+      this.mustHaveIntegerOperands();
+      this.type = Type.NUMBER;
+    }
+  }
+
+  mustHaveIntegerOperands() {
+    const errorMessage = `${this.op} must have integer operands`;
+    this.left.type.mustBeCompatibleWith(Type.NUMBER, errorMessage, this.op);
+    this.right.type.mustBeCompatibleWith(Type.NUMBER, errorMessage, this.op);
+  }
+  mustHaveBooleanOperands() {
+    const errorMessage = `${this.op} must have boolean operands`;
+    this.left.type.mustBeCompatibleWith(Type.BOOLEAN, errorMessage, this.op);
+    this.right.type.mustBeCompatibleWith(Type.BOOLEAN, errorMessage, this.op);
+  }
+  mustHaveCompatibleOperands() {
+    const errorMessage = `${this.op} must have mutually compatible operands`;
+    this.left.type.mustBeMutuallyCompatibleWith(this.right.type, errorMessage, this.op);
   }
 
   optimize() {
