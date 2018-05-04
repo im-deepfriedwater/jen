dc7bddc3ba1e8e4b0a4f5b8494583a5abb2166eb
CatLoverKid <aclkeba@gmail.com>
2018-05-03 17:45:03 -0700

    fixed code generation tests to work using backend gen method

--- test/test-generation.js
+++ test/test-generation.js
@@ -5,11 +5,10 @@
  * https://github.com/rtoal/plainscript/blob/master/test/analyzer/analyzer-test.js
  *
  */
-
+require('../backend/python-generator');
 const fs = require('fs');
 const assert = require('assert');
-const PARSE = require('../syntax/parser');
-const GENERATE = require('../backend/python-generator');
+const parse = require('../syntax/parser');
 const PYTHON_BRIDGE = require('python-bridge');
 
 const JEN_PROGRAMS_DIR = 'test/data/code-generation/jen-programs';
@@ -18,17 +17,15 @@ const PYTHON_PROGRAMS_DIR = 'test/data/code-generation/python-programs';
 const pythonResults = [];
 
 describe('Code Generation', () => {
-  beforeEach(() => {
-    fs.readdirSync(PYTHON_PROGRAMS_DIR).forEach((name) => {
-      PYTHON_BRIDGE(fs.readFileSync(`${PYTHON_PROGRAMS_DIR}/${name}`, 'utf-8')).then((x) => {
-        pythonResults.append(x);
-      });
+  fs.readdirSync(PYTHON_PROGRAMS_DIR).forEach((name) => {
+    PYTHON_BRIDGE(fs.readFileSync(`${PYTHON_PROGRAMS_DIR}/${name}`, 'utf-8')).then((x) => {
+      pythonResults.append(x);
     });
   });
 
   fs.readdirSync(JEN_PROGRAMS_DIR).forEach((name) => {
     it(`should analyze ${name} without errors`, () => {
-      const program = GENERATE(PARSE(fs.readFileSync(`${JEN_PROGRAMS_DIR}/${name}`, 'utf-8')).analyze());
+      const program = parse(fs.readFileSync(`${JEN_PROGRAMS_DIR}/${name}`, 'utf-8')).gen();
       assert.equal(pythonResults[0], program);
       pythonResults.splice(0, 1);
     });
